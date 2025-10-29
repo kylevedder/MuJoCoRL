a8586b7e56bb1c019f282e194ec92cfe03094859
Kyle Vedder <kyle.c.vedder@gmail.com>
2025-10-29 06:46:42 +0000

    Added proper versions for getting Fetch pick and place working

--- demo.py
+++ demo.py
@@ -1,10 +1,11 @@
 import os
 from pathlib import Path
 import gymnasium as gym
+import gymnasium_robotics as gym_robotics
 import matplotlib.pyplot as plt
 
 print("MUJOCO_GL:", os.environ.get("MUJOCO_GL"))
-env = gym.make("FetchPickAndPlace-v3", render_mode="rgb_array")
+env = gym.make("FetchPickAndPlace-v4", render_mode="rgb_array", width=640, height=480)
 env.reset()
 frame = env.render()
 print("frame:", frame.shape)
